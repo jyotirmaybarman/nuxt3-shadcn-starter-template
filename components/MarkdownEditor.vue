8234fae2634f31d95ce97d87e1a6ecfb5f93d467
Jyotirmay Barman <jyotirmaybarman.email@gmail.com>
2021-12-14 15:05:34 +0530

    modify: markdown editor

--- components/MarkdownEditor.vue
+++ components/MarkdownEditor.vue
@@ -4,9 +4,9 @@
             <button type="button" class="py-2 px-4 inline-block font-semibold text-gray-400 hover:text-black dark:hover:text-white" :class="editing ? 'dark:hover:text-indigo-500 hover:text-indigo-500 text-indigo-500':''" @click="editing=true">Write</button>
             <button type="button" class="py-2 px-4 inline-block text-gray-400 font-semibold hover:text-black dark:hover:text-white" :class="!editing ? 'dark:hover:text-indigo-500 hover:text-indigo-500 text-indigo-500':''" @click="renderHtml">Preview</button>
         </div>
-        <div class="w-full relative rounded-b-md shadow-sm border border-gray-300 dark:border-gray-800 h-[500px] overflow-y-auto dark:bg-[#1c1c1c]" >
+        <div class="w-full relative rounded-b-md shadow-sm border border-gray-300 dark:border-gray-800 h-[500px] overflow-y-auto" >
             <textarea class="w-full h-full block p-4 outline-none resize-none dark:bg-[#1c1c1c]" v-model="md" v-if="editing" placeholder="# Hello World !!"></textarea>
-            <div class="preview h-max p-4 top-0 left-0 prose md:prose-md lg:prose-lg xl:prose-xl block max-w-none prose-indigo leading-6 dark:prose-dark dark:bg-[#1c1c1c]" v-html="rendered" v-else></div>
+            <div class="preview h-max min-h-full p-4 top-0 left-0 prose md:prose-md lg:prose-lg xl:prose-xl block max-w-none prose-indigo leading-6 dark:prose-dark dark:bg-[#1c1c1c]" v-html="rendered" v-else></div>
         </div>
     </div>
 </template>
