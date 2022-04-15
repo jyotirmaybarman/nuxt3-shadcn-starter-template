174cfa6b77904314eb9e1cbcc625ff319454a62b
Jyotirmay Barman <jyotirmaybarman.email@gmail.com>
2022-04-15 20:08:48 +0530

    add navbar & footer components

---
+++ components/TheFooter.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer class="border dark:border-gray-800 h-12 flex justify-center items-center">
+        The Footer
+    </footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="postcss" scoped>
+
+</style>
---
+++ components/TheNavbar.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav class="border dark:border-gray-800 h-12 flex justify-center items-center">
+        The Navbar
+    </nav>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="postcss" scoped>
+
+</style>
