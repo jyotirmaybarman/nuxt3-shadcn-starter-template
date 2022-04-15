0143334c5081a1271e1d3ce7912f0ff7ec9ba1b7
Jyotirmay Barman <jyotirmaybarman.email@gmail.com>
2022-04-15 20:09:05 +0530

    add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="default-layout h-screen container mx-auto flex flex-col">
+        <the-navbar />
+        <div class="main-content flex-auto border dark:border-gray-800">
+            <slot />
+        </div>
+        <the-footer />
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="postcss" scoped>
+
+</style>
