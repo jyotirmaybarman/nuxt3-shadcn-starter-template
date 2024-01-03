d135e2e5f4c289ee679614f7ae32f344dd7c4d5f
Jyotirmay Barman <jyotirmay@developerzilla.com>
2024-01-03 15:47:16 +0530

    fix: dark mode in markdown editor

--- components/MarkdownEditor.vue
+++ components/MarkdownEditor.vue
@@ -6,7 +6,7 @@
         </div>
         <div class="w-full relative rounded-b-md shadow-sm border border-gray-300 dark:border-gray-800 h-[500px] overflow-y-auto" >
             <textarea class="w-full h-full block p-4 outline-none resize-none dark:bg-[#1c1c1c]" v-model="md" v-if="editing" placeholder="# Hello World !!"></textarea>
-            <div class="preview h-max min-h-full p-4 top-0 left-0 prose md:prose-md lg:prose-lg xl:prose-xl block max-w-none prose-indigo leading-6 dark:prose-dark dark:bg-[#1c1c1c]" v-html="rendered" v-else></div>
+            <div class="preview h-max min-h-full p-4 top-0 left-0 prose md:prose-md lg:prose-lg xl:prose-xl block max-w-none prose-indigo leading-6 dark:prose-invert dark:bg-[#1c1c1c]" v-html="rendered" v-else></div>
         </div>
     </div>
 </template>
